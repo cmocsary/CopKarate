5f90d747c24c86ac0fececf4c713c9d49da231ac
cmocsary <carlos.mocsary@gmail.com>
2024-10-22 21:33:52 -0300

    se agregan modificaciones al karate-config.js para la configuracion del driver

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,14 +1,31 @@
 function fn() {
-    karate.configure('connectTimeout', 10000);
-    karate.configure('readTimeout', 10000);
 
-    return {
-        web: {
-           baseUrl: 'https://www.saucedemo.com/'
-        },
-        user: {
+    karate.configure('connectTimeout', 30000);
+    karate.configure('readTimeout', 30000);
+
+    var config = {
+            baseUrl: 'https://www.saucedemo.com/',
             name: 'standard_user',
-            pass: 'secret_sauce'
-        }
-    };
+            pass: 'secret_sauce',
+            driver: {
+                type: 'chromedriver',
+                headless: false,
+                start: true,
+                showDriverLog: true,
+                webDriverUrl: null,
+                executable: './drivers/chromedriver.exe',
+                //port: 9223,
+                capabilities: {
+                            'alwaysMatch':{
+                            'browserName':'chrome',
+                            'goog:chromeOptions': {
+                                args: ['--disable-infobars', '--window-size=1200,800']
+                                //addOptions: ['--disable-save-password-bubble'] deshabilitar password chrome
+                            }
+                        }
+
+                }
+            }
+        };
+    return config;
 }
