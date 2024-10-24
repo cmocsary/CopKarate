a5d9dc428db5acd3ba0e81408e8c415949242c08
fbriz <fbzbkr@gmail.com>
2024-10-23 21:54:52 -0300

    se elimina la funcion pressEnter.js

--- src/test/java/cop/ui/pressEnter.js
+++
@@ -1,7 +0,0 @@
-function pressEnter() {
-  var robot = Java.type('java.awt.Robot');
-  var keyEvent = Java.type('java.awt.event.KeyEvent');
-  var r = new robot();
-  r.keyPress(keyEvent.VK_ENTER);
-  r.keyRelease(keyEvent.VK_ENTER);
-}
